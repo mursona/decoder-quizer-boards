371c55858e53d3fec5ed52ff09e7528498dad2d7
mursona <mursona>
2022-10-12 00:31:22 +0600

    total quiz statistics responsive chart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useLoaderData } from "react-router-dom";
-import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Statistics = () => {
     const topics = useLoaderData();
     const data = topics.data;
     console.log(data);
     return (
-      <div className="stats">
-        <h2>Quiz Statistics</h2>
-        <div className="chart">
-          <LineChart width={300} height={300} data={data}>
+      <div>
+          <h2 className='mt-6 underline font-bold text-blue-900 text-3xl'>Total Question Statistics - Each Courses</h2>
+          <div className="chart my-12 w-full flex flex-col sm:flex-col md:flex-row mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl align-middle justify-center">
+          <LineChart width={450} height={400} data={data}>
+          <CartesianGrid stroke="#3a92e4" />
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip></Tooltip>
@@ -22,7 +23,6 @@ const Statistics = () => {
             />
           </LineChart>
         </div>
-        <p>Total Available Questions</p>
       </div>
     );
   };
